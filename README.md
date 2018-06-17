c1f10a6de1f0aef7df4a26cbb6ac7ac843de5b1f
Florestan Lefevre <florestanlefevre@gmail.com>
2018-06-17 15:02:01 -0700

    Add color direction update

---
+++ README.md
@@ -0,0 +1,9 @@
+# Background Generator
+
+*Couldn't find a simple linear-gradient generator online so I made one to save time in my future projects*
+
+## Preview
+
+<p align="center">
+  <img src="https://github.com/floustao/background-generator/blob/master/gradient.gif" width="400" height="250"/>
+</p>
--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const color1 = document.querySelector("[name='color1']");
 const color2 = document.querySelector("[name='color2']");
 const h3 = document.querySelector('h3');
 let body = document.querySelector('body');
+let direction = document.querySelector("[name='direction']");
 
 function updateColor() {
-  body.style.background = `linear-gradient(to right, ${color1.value} , ${color2.value}`;
+  body.style.background = `linear-gradient(${direction.value}deg, ${color1.value}, ${color2.value}`;
   h3.textContent = body.style.background + ";";
 }
 
 color1.addEventListener('input', updateColor);
 color2.addEventListener('input', updateColor);
+direction.addEventListener('input', updateColor);
